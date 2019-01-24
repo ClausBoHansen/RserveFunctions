273e2a3d154d296537116fc5e372c904a3699a8c
Claus Bo Hansen <sbo@trolling.dk>
2019-01-24 16:08:14 +0100

    First commit from R Studio

---
+++ R/prediction.R
@@ -0,0 +1,14 @@
+#' @title Make prediction based on saved model
+#'
+#' @description Loads specified model and makes prediction using given predictors
+#' @param model Model already loaded in environment
+#' @keywords machine_learning prediction
+#' @export
+#' @examples
+#' coming ...
+
+# Define function Prediction
+
+Prediction <- function() {
+  17
+}
